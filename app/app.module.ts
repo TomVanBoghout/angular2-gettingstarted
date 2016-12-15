7aaf26501cbdc7114d22dbdb5355e980ebd0d97c
tomvanboghout <tvboghout@gmail.com>
2016-12-15 11:19:13 +0100

    Finished - Navigating and Routing basics

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,18 +7,30 @@ import {FormsModule} from "@angular/forms";
 import {ProductFilterPipe} from "./products/product-filter.pipe";
 import {StarComponent} from "./shared/star.component";
 import {HttpModule} from "@angular/http";
+import {WelcomeComponent} from "./home/welcome.component";
+import {ProductDetailComponent} from "./products/product-detail.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   imports: [
       BrowserModule,
       FormsModule,
-      HttpModule
+      HttpModule,
+      RouterModule.forRoot([
+          {path: 'products', component: ProductListComponent},
+          {path: 'products/:id', component: ProductDetailComponent},
+          {path: 'welcome', component: WelcomeComponent},
+          {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+          {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+      ])
   ],
   declarations: [
       AppComponent,
       ProductListComponent,
       ProductFilterPipe,
-      StarComponent
+      StarComponent,
+      WelcomeComponent,
+      ProductDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
